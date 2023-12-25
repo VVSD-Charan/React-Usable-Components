e6d19e1474a031ab4439b8a4083810efd3f68c6d
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-12-25 20:00:08 +0530

    modified:   src/components/Navbar/Dropdown.js 	modified:   src/components/Navbar/MenuItems.js

--- src/components/Navbar/Dropdown.js
+++ src/components/Navbar/Dropdown.js
@@ -1,12 +1,33 @@
 import React,{useState} from 'react'
-import MenuItems from './MenuItems.js'
+import {MenuItems} from './MenuItems.js'
 import {Link} from 'react-router-dom'
 import './Dropdown.css'
 
 function Dropdown(){
+
+    const [click,setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+
+
     return (
         <>
-            <h1>Dropdown</h1>
+            <ul onClick={handleClick} className={
+                click? 'dropdown-menu clicked':'dropdown-menu'
+            }>
+                {
+                    MenuItems.map((item,index)=>{
+                        return (
+                            <li key={index}>
+                                <Link className={item.className}
+                                to={item.path} onClick={()=>setClick(false)}>
+                                    {item.title}
+                                </Link>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </>
     )
 };
--- src/components/Navbar/MenuItems.js
+++ src/components/Navbar/MenuItems.js
@@ -1,11 +1,22 @@
-import React from 'react'
-
-function MenuItems(){
-    return (
-        <>
-            <h1>Menu Items</h1>
-        </>
-    )
-};
-
-export default MenuItems;
+export const MenuItems = [
+    {
+        title : 'Option1',
+        path : '/option1',
+        className : 'dropdown-link'
+    },
+    {
+        title : 'Option2',
+        path : '/option2',
+        className : 'dropdown-link'
+    },
+    {
+        title : 'option3',
+        path : '/option3',
+        className : 'dropdown-link'
+    },
+    {
+        title : 'option4',
+        path : '/option4',
+        className : 'dropdown-link'
+    }
+]
