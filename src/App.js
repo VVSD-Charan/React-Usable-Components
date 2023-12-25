0100f01e55dc3b7d8c27f8c235f21b1aade60068
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-12-25 20:24:46 +0530

    modified:   src/App.css 	modified:   src/App.js 	new file:   src/components/Navbar/pages/Consulting.js 	new file:   src/components/Navbar/pages/Contactus.js 	new file:   src/components/Navbar/pages/Home.js 	new file:   src/components/Navbar/pages/Marketing.js 	new file:   src/components/Navbar/pages/Products.js 	new file:   src/components/Navbar/pages/Services.js 	new file:   src/components/Navbar/pages/Signup.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar/Navbar.js'
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+
+import Consulting from './components/Navbar/pages/Consulting.js';
+import Services from './components/Navbar/pages/Services.js';
+import Home from './components/Navbar/pages/Home.js';
+import Products from './components/Navbar/pages/Products.js';
+import Marketing from './components/Navbar/pages/Marketing.js';
+import Contactus from './components/Navbar/pages/Contactus.js';
 
 function App() {
   return (
     <Router>
       <Navbar/>
+
+      <Routes>
+         <Route path='/' Component={Home}/>
+         <Route path='/services' Component={Services}/>
+         <Route path='/contact-us' Component={Contactus}/>
+         <Route path='/marketing' Component={Marketing}/>
+         <Route path='/products' Component={Products}/>
+         <Route path='/consulting' Component={Consulting}/>
+      </Routes>
     </Router>
   );
 }
