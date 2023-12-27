0bd6ed5867df7894cff8ad4074a469623de617b0
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-12-27 14:13:57 +0530

    modified:   src/components/Accordion/Accordion.js

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,105 +1,97 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import { Accordiondata } from './Accordiondata';
-import {IconContext} from 'react-icons';
-import {FiPlus,FiMinus} from 'react-icons/fi';
-import styled from 'styled-components'
+import { IconContext } from 'react-icons';
+import { FiPlus, FiMinus } from 'react-icons/fi';
+import styled from 'styled-components';
 
 const AccordianSection = styled.div`
-    display : flex;
-    flex-direction : column;
-    align-items : center;
-    justify-content : center;
-    position : relative;
-    height : 100vh;
-    background : #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end; /* Updated to move the component to the bottom */
+  position: relative;
+  height: 100vh;
+  background: #fff;
 `;
 
 const Container = styled.div`
-    position : absolute;
-    top : 30%;
-    box-shadow : 2px 10px 35px 1px rgba(153,153,153,0.3);
-    width : 100%;
+  position: absolute;
+  bottom: 0; /* Updated to stick to the bottom */
+  box-shadow: 2px 10px 35px 1px rgba(153, 153, 153, 0.3);
+  width: 100%;
 `;
 
 const Wrap = styled.div`
-    background : #272727;
-    color : #fff;
-    display : flex;
-    justify-content : space-between;
-    align-items : center;
-    width : 100%;
-    text-align : center;
-    cursor : pointer;
+  background: #272727;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+  cursor: pointer;
 
-    h1{
-        padding : 17px;
-        font-size : 24px;
-    }
+  h1 {
+    padding: 17px;
+    font-size: 24px;
+  }
 
-    span{
-        margin-right : 1.5rem;
-    }
+  span {
+    margin-right: 1.5rem;
+  }
 `;
 
 const Dropdown = styled.div`
-    background : #1c1c1c;
-    color : #00ffb9;
-    width : 100%;
-    height : 100px;
-    display : flex;
-    flex-direction : column;
-    justify-content : center;
-    align-items : center;
-    border-bottom : 1px solid #00ffb9;
-    border-top : 1px solid #00ffb9;
+  background: #1c1c1c;
+  color: #00ffb9;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid #00ffb9;
+  border-top: 1px solid #00ffb9;
 
-    p{
-        font-size : 20px;
-    }
+  p {
+    font-size: 20px;
+  }
 `;
 
-
 const Accordion = () => {
+  const [clicked, setClicked] = useState(false);
 
-    const [clicked,setClicked] = useState(false);
-
-    const toggle = index =>{
-        if(clicked === index){
-            return setClicked(null);
-        }
-        setClicked(index)
+  const toggle = (index) => {
+    if (clicked === index) {
+      return setClicked(null);
     }
+    setClicked(index);
+  };
 
   return (
-    <IconContext.Provider value={{color : '#00FFB9',size : '25px'}}>
-        <AccordianSection>
-            <Container>
-                {
-                    Accordiondata.map((item,index)=>{
-                        return (
-                            <>
-                                <Wrap onClick={()=>toggle(index)} key={index}>
-                                    <h1>{item.question}</h1>
-                                    <span>
-                                        {
-                                            clicked === index? <FiMinus/>:<FiPlus/>
-                                        }
-                                    </span>
-                                </Wrap>
-                                
-                                {
-                                    clicked === index ? (<Dropdown>
-                                        <p>{item.answer}</p>
-                                    </Dropdown>):null
-                                }
-                            </>
-                        );
-                    })
-                }
-            </Container>
-        </AccordianSection>
+    <IconContext.Provider value={{ color: '#00FFB9', size: '25px' }}>
+      <AccordianSection>
+        <Container>
+          {Accordiondata.map((item, index) => {
+            return (
+              <>
+                <Wrap onClick={() => toggle(index)} key={index}>
+                  <h1>{item.question}</h1>
+                  <span>{clicked === index ? <FiMinus /> : <FiPlus />}</span>
+                </Wrap>
+
+                {clicked === index ? (
+                  <Dropdown>
+                    <p>{item.answer}</p>
+                  </Dropdown>
+                ) : null}
+              </>
+            );
+          })}
+        </Container>
+      </AccordianSection>
     </IconContext.Provider>
-  )
-}
+  );
+};
 
 export default Accordion;
