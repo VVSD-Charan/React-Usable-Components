38ef01eb2ee8e5c50388a1c283bfd5d14a735c53
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-12-27 20:48:09 +0530

    modified:   src/App.css 	modified:   src/components/Navbar/pages/Services.js 	modified:   src/components/webdata.js

--- src/components/Navbar/pages/Services.js
+++ src/components/Navbar/pages/Services.js
@@ -1,6 +1,44 @@
-import React from 'react';
-import '../../../App.css';
+import React from 'react'
+import '../../../App.css'
+import { services_content } from '../../webdata';
 
-export default function Services() {
-  return <h1 className='services'>SERVICES</h1>;
+const Services = () => {
+
+    return (
+        <section className='section-white'>
+            <div className='container'>
+                <div className='row'>
+
+                    {/* About us heading and content  */}
+                    <div className='col-md-12 text-center'>
+                        <h2 className='section-title'>
+                            Services
+                        </h2>
+
+                    </div>
+
+                    {/* Cards layout  */}
+                    <div className='container'>
+                    {
+                        services_content.map((item, index) => {
+                            return (
+                                <div className='card'>
+                                    <div className='team-item' key={index}>
+                                        <img src={item.image} alt={item.title} className='team-img' />
+                                        <h3>{item.title}</h3>
+                                        <div className='team-info'>
+                                            <p>{item.description}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
+
+export default Services;
--- src/components/webdata.js
+++ src/components/webdata.js
@@ -1,3 +1,5 @@
+// About us content 
+
 export const aboutus_content = "We began our company in Telangana, Hyderabad, aiming to greatly benefit our customers by offering a complete platform for all essential services and resources needed to start a new business. Our goal is to simplify the process by providing a one-stop solution for business growth, including services such as generating business ideas, managing finances, handling documentation, registration, legal services, and accessing government business loans, all in one place. This approach aims to enhance efficiency and accessibility, potentially boosting the success of aspiring entrepreneurs. Moreover, by fostering and supporting entrepreneurship, our company seeks to contribute to economic growth and job creation, ultimately benefiting society as a whole.";
 
 export const whyus = [
@@ -20,5 +22,29 @@ export const whyus = [
         title : "Multiple lenders",
         image : "https://www.advisoryexcellence.com/wp-content/uploads/2022/02/CASH-LOAN-PHOTO.jpg",
         description : "We have the strong relationships with the multiple public banks, private banks,nbfc,and other credit societies.This will give the strong funding option for the customer and all the services offered by lono will definitely make it easy for the lenders to process out the  process for credit"
+    },
+    {
+        title : "Government schemes",
+        image : "https://payu.in/blog/wp-content/uploads/2022/07/government-schemes-for-business.png",
+        description : "We under take all the eligibile  schemes for the business loan process to benifit the customer and make use of all the subsidiary schemes for the all group of people in India . Customers cannot fill the requirements for the bankers to get loan easily .Butwe  make it easier for our customers"
+    }
+];
+
+// Services content 
+export const services_content = [
+    {
+        title : "Documentation",
+        description : "All the documents,aggrements are made perfectly by our professionals",
+        image : "https://fatstacksblog.com/wp-content/uploads/2020/10/business-docs-oct21.jpg",
+    },
+    {
+        title : "Registration",
+        description : "All the business rigistrations, licences ,tax compliance trade mark ,GST ..etc are Done by our professionals on Time.",
+        image : "https://okcredit-blog-images-prod.storage.googleapis.com/2021/01/registration11.jpg",
+    },
+    {
+        title : "Legal services",
+        description : "All the legal services of government are done by our legal department Lawyers,charted accountants at in Time.",
+        image : "https://www.hoffmanforde.com/wp-content/uploads/2021/02/business-legal-advisory-services-scaled.jpeg",
     }
 ]
