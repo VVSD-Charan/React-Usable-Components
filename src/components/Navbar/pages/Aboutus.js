504428586dedac0e5c273c3ede11c349611d94ab
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-12-27 18:53:27 +0530

    modified:   public/index.html 	modified:   src/App.css 	modified:   src/components/Navbar/pages/Aboutus.js 	new file:   src/components/webdata.js

--- src/components/Navbar/pages/Aboutus.js
+++ src/components/Navbar/pages/Aboutus.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import '../../../App.css'
+import { aboutus_content, whyus } from '../../webdata';
+
 
 const Aboutus = () => {
-  return (
-    <h1 className='aboutus'>About us</h1>
-  )
+
+    return (
+        <section className='section-white'>
+            <div className='container'>
+                <div className='row'>
+
+                    {/* About us heading and content  */}
+                    <div className='col-md-12 text-center'>
+                        <h2 className='section-title'>
+                            About us
+                        </h2>
+
+                        <p className='section-subtitle'>
+                            {aboutus_content}
+                        </p>
+                    </div>
+
+                    {/* Cards layout  */}
+                    {
+                        whyus.map((item, index) => {
+                            return (
+                                <div className='col-sm-6 col-md-4'>
+                                    <div className='team-item' key={index}>
+                                        <img src={item.image} alt={item.title} className='team-img' />
+                                        <h3>{item.title}</h3>
+                                        <div className='team-info'>
+                                            <p>{item.description}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Aboutus;
---
+++ src/components/webdata.js
@@ -0,0 +1,24 @@
+export const aboutus_content = "This is the content";
+
+export const whyus = [
+    {
+        title : "Business management",
+        image : "https://viralrang.com/wp-content/uploads/2019/02/Business-Management.jpg",
+        description : "We have all the professionals who can work for the company efficiently in documentation, rigistration and legal services .this will enable the process faster than reaching out individually by customers.",
+    },
+    {
+        title : "Project report",
+        image : "https://img.favpng.com/19/0/19/annual-report-business-company-information-png-favpng-M6fCJ0F2F8YuRmAQFEz0BbniB.jpg",
+        description : " 90% of business loans fail due to bad project report . We have the best professionals who can give strong report on business.we go through practical knowledge experts in that business.this will give the report 100 % valuable information irrespective of place of business ."
+    },
+    {
+        title : "Business setup",
+        image : "",
+        description : "All the requirements of the project like machinary, infrastructure,raw materials, products, distributions ..etc are directly interconnected to our company . So that it is easy to estimate the project cost and make faster collaborations in between the parties.this will give the trust to the lender banks or nbfc."
+    },
+    {
+        title : "Multiple lenders",
+        image : "",
+        description : "We have the strong relationships with the multiple public banks, private banks,nbfc,and other credit societies.This will give the strong funding option for the customer and all the services offered by lono will definitely make it easy for the lenders to process out the  process for credit"
+    }
+]
