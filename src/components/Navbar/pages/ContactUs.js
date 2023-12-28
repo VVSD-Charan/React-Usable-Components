93b611218d3abff5966cceb20c8f893b68ed8bdf
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-12-28 18:11:28 +0530

    new file:   src/components/Navbar/pages/Contact.css 	modified:   src/components/Navbar/pages/ContactUs.js

--- src/components/Navbar/pages/ContactUs.js
+++ src/components/Navbar/pages/ContactUs.js
@@ -1,6 +1,27 @@
 import React from 'react';
-import '../../../App.css';
+import './Contact.css';
 
 export default function ContactUs() {
-  return <div className='contact-us'>CONTACT</div>;
+  return (
+    <section className='App'>
+        <div className='register'>
+          <div className='col-1'>
+              <h2>Get it touch</h2>
+              <span>Please describe about your queries clearly</span>
+
+              <form id='form' className='flex flex-col'>
+                 <input type='text' placeholder='Name'/>
+                 <input type='number' placeholder='Contact number'/>
+                 <input type='text' placeholder='Message'/>
+
+                 <button className='bton'>Sign In</button>
+              </form>
+          </div>
+
+          <div className='col-2'>
+
+          </div>
+        </div>
+    </section>
+  )
 }
