e913e31a6c13a01dc1349588ba4a730c391b47d4
pavankumar130 <20bcs130@iiitdmj.ac.in>
2024-01-01 10:35:47 +0530

    modified:   src/components/Navbar/pages/ContactUs.js

--- src/components/Navbar/pages/ContactUs.js
+++ src/components/Navbar/pages/ContactUs.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import './Contact.css';
+import React from 'react'
+import './Contact.css'
 
 export default function ContactUs() {
   return (
-    <section className='App'>
-        <div className='register'>
-          <div className='col-1'>
-              <h2>Get it touch</h2>
-              <span>Please describe about your queries clearly</span>
+    <section className="App">
+      <div className="register">
+        <div className="col-1">
+          <h2>Get in touch</h2>
+          <span>Please describe your queries clearly</span>
 
-              <form id='form' className='flex flex-col'>
-                 <input type='text' placeholder='Name'/>
-                 <input type='number' placeholder='Contact number'/>
-                 <input type='text' placeholder='Message'/>
+          <form id="form" className="flex flex-col">
+            <input type="text" placeholder="Name" />
+            <input type="number" placeholder="Contact number" />
+            <input type="text" placeholder="Message" />
 
-                 <button className='bton'>Sign In</button>
-              </form>
-          </div>
-
-          <div className='col-2'>
-
-          </div>
+            <button className="bton">Contact Us</button>
+          </form>
         </div>
+
+        <div className="col-2">
+          {/* You can add content for the second column if needed */}
+        </div>
+      </div>
     </section>
   )
 }
