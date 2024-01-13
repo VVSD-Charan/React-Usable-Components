ee0ba1c84e877b0faecb8053871640247c8f1555
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2024-01-14 00:05:38 +0530

    Added data

--- src/components/services/Automobile_business.js
+++ src/components/services/Automobile_business.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Automobile_business = () => {
   return (
-    <div className='home'>Automobile_business</div>
+    <div className='home automobile'>
+      <div>
+        <p>
+          Commercial vehicles, transportation business became a vital role in infrastructure development of country. There is huge demand for different types of commercial business vehicles like trucks, lorries, jcb, itachi, tractors, buses, cranes, cars, autos in the business segment of different needs like goods transportation, infrastructure development, transportation for people to meet the needs of people. These market demand for the transportation create opportunity for the new entrepreneurs existance. Commercial automobiles and the transportation industry have grown to be essential to the nation's infrastructural development. Different kinds of commercial business vehicles, such as trucks, lorries, JCB, Hitachi, tractors, buses, cranes, cars, and autos, are in high demand in the business sector for a variety of purposes, including the movement of commodities, the construction of infrastructure, and the provision of transportation for people to satisfy their demands. The market's need for mobility generates opportunities for the emergence of new enterprises.
+
+        </p>
+      </div>
+    </div>
   )
 }
 
