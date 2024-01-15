5afd27f1d1d0e0c0d1353efda6a772eaaa3eb7c4
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2024-01-15 15:15:13 +0530

    Added better styles and data

--- src/components/services/Manufacturing_business.js
+++ src/components/services/Manufacturing_business.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Manufacturing_business = () => {
   return (
-    <div className='home'>Manufacturing_business</div>
+    <div className='home manufacturing'>
+      <div>
+        <p>
+          Starting a new manufacturing business comes with various challenges. Securing funds for equipment, operations, and initial costs can be tough, making financial management crucial. Choosing the right location, considering infrastructure, logistics, and accessibility, is vital. Maintaining a steady supply chain of raw materials is critical for uninterrupted production. Setting competitive prices, identifying your target market, and building strong customer relationships are essential for success. Navigating through legal procedures, permits, and contracts can be daunting, requiring legal services for compliance and protection of intellectual property. Recognizing market trends, growth opportunities, and potential partnerships helps businesses stay competitive and adapt to industry changes. Effective risk management, considering market shifts and technological advancements, is key for long-term sustainability. Success in manufacturing requires thorough market research, a robust business plan, and a proactive problem-solving approach.
+        </p>
+      </div>
+    </div>
   )
 }
 
