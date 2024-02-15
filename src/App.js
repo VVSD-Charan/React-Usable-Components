aeb3108de07a4bfcff2fa64c23e1e1965ab372c3
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2024-02-15 15:08:42 +0530

    Added all pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo from './logo.svg';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Home from '../src/home/Home.js'
+import Careers from './Careers.js';
 import './App.css';
+import SDE from './roles/SDE.js';
+import Manager from './roles/Manager.js';
+import Services from './Services.js';
+import Agriloan from './services/Agriloan.js';
+import Team from './Team.js';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Home />}>
-        </Route>
+          <Route path="/" element={<Home />}/>
+          <Route path="/careers" element={<Careers/>}/>
+          <Route path="/careers-developer" element={<SDE/>}/>
+          <Route path="/careers-manager" element={<Manager/>}/>
+          <Route path="/services" element={<Services/>}/>
+          <Route path="/agriculture-loan" element={<Agriloan/>}/>
+          <Route path="/team" element={<Team/>}/>
       </Routes>
     </BrowserRouter>
   );
