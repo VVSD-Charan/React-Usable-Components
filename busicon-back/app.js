477e59daf6b2ebd65735ff85d406a373fff413fe
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2024-03-01 18:50:50 +0530

    Added new job

--- busicon-back/app.js
+++ busicon-back/app.js
@@ -19,29 +19,35 @@ async function createJob(jobData) {
     }
   }
   
-  // const jobData = {
-  //   jobId: uniqid(),
-  //   jobDescription: ['Add animations to web pages.', 
-  //       'Make pages responsive for all screens.',
-  //       'Ensure code re-usability.',
-  //       'Make responsive web pages using React.',
-  //       'Knowledge of Material UI is a plus.'
-  //   ],
-  //   jobTitle: 'Front End Developer Intern',
-  //   jobLocation: 'Siddipet / Remote',
-  //   jobRequired : [
-  //       'Good knowledge of JavaScript and React life cycle .',
-  //       'Good communication skills and knowlede about SDLC',
-  //       'Should be good in using JavaScript DOM'
-  //   ]
-  // };
+  const jobData = {
+    jobId: uniqid(),
+    jobDescription: ['Add animations to web pages.', 
+        'Optimize API performance.',
+        'Work with both SQL and NoSQL databases',
+        'Work with cloud services like AWS, Azure',
+        'Make pages responsive for all screens.',
+        'Ensure code re-usability.',
+        'Make responsive web pages using React.',
+        'Knowledge of Material UI is a plus.'
+    ],
+    jobTitle: 'Full Stack Developer Intern',
+    jobLocation: 'Siddipet',
+    jobRequired : [
+        'Good knowledge of JavaScript and React life cycle .',
+        'Good communication skills and knowlede about SDLC',
+        'Should be good in using JavaScript DOM',
+        'Good problem solving skills',
+        'Proficient in atleast one programming language like C/C++/Python',
+        'Past internship / work experience is a plus.'
+    ]
+  };
   
   database.on('error', console.error.bind(console, 'MongoDB connection error:'));
-  // database.once('open', async () => {
-  //   console.log('Connected to MongoDB');
+  database.once('open', async () => {
+    console.log('Connected to MongoDB');
   
-  //   await createJob(jobData);
-  // });
+    await createJob(jobData);
+  });
 
 app.get('/alljobs', async (req, res) => {
     try {
