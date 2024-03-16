8ec6026c78a7c9aa552bded42bbb1dc73d2a6409
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2024-03-16 18:15:03 +0530

    Added admin security

--- src/AddJob.js
+++ src/AddJob.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios';
 
 const AddJob = () => {
+
+    const [loggedIn, setLoggedIn] = useState(false);
 
     async function handleSubmit(e) {
         e.preventDefault();
@@ -19,60 +21,102 @@ const AddJob = () => {
             };
             const resp = await axios.post('http://localhost:3001/addjob', null, config);
 
-            if(resp && resp?.status === 200)
-            {
-                alert("Job created successfully");
-            }
+            console.log(resp);
         } catch (error) {
             console.log(error);
+        }
+    }
+
+    async function handleLogin(e)
+    {
+        e.preventDefault();
+
+        const password = e.target.adminPassword.value;
+
+        if(password == process.env.REACT_APP_ADMIN_PASSWORD)
+        {
+            setLoggedIn(true);
+        }
+        else
+        {
+            alert("Password is wrong!");
         }
     }
 
 
     return (
         <div className="container pt-120">
-            <div className="row gx-0">
-                <div className="col-xl-7 col-lg-6">
-                    <div className="contact-form-right-warp">
-                        <div className="postbox__comment-form">
-                            <p style={{textAlign:'center',fontSize:'30px'}}>Add a job</p>
-                            <form className="box" onSubmit={handleSubmit}>
-                                <div className="row gx-20">
-                                    <div className="col-12">
-                                        <div className="postbox__comment-input mb-30">
-                                            <input type="text" name='jobTitle' className="inputText" required />
-                                            <span className="floating-label">Job Title</span>
+            {
+                loggedIn ? (
+                    <div className="row gx-0">
+                        <div className="col-xl-7 col-lg-6">
+                            <div className="contact-form-right-warp">
+                                <div className="postbox__comment-form">
+                                    <p style={{ textAlign: 'center', fontSize: '30px',marginBottom:'20px' }}>Add a job</p>
+                                    <form className="box" onSubmit={handleSubmit}>
+                                        <div className="row gx-20">
+                                            <div className="col-12">
+                                                <div className="postbox__comment-input mb-30">
+                                                    <input type="text" name='jobTitle' className="inputText" required />
+                                                    <span className="floating-label">Job Title</span>
+                                                </div>
+                                            </div>
+                                            <div className="col-12">
+                                                <div className="postbox__comment-input mb-30">
+                                                    <input type="text" name='jobDescription' className="inputText" required />
+                                                    <span className="floating-label">Job Description</span>
+                                                </div>
+                                            </div>
+                                            <div className="col-12">
+                                                <div className="postbox__comment-input mb-30">
+                                                    <input type="text" name='jobRequired' className="inputText" required />
+                                                    <span className="floating-label">Job Requirements</span>
+                                                </div>
+                                            </div>
+                                            <div className="col-12">
+                                                <div className="postbox__comment-input mb-30">
+                                                    <input type="text" name='jobLocation' className="inputText" required />
+                                                    <span className="floating-label">Job Location</span>
+                                                </div>
+                                            </div>
+                                            <div className="col-xxl-12">
+                                                <div className="postbox__btn-box">
+                                                    <button type='submit' className="submit-btn w-100">Add job</button>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="col-12">
-                                        <div className="postbox__comment-input mb-30">
-                                            <input type="text" name='jobDescription' className="inputText" required />
-                                            <span className="floating-label">Job Description</span>
-                                        </div>
-                                    </div>
-                                    <div className="col-12">
-                                        <div className="postbox__comment-input mb-30">
-                                            <input type="text" name='jobRequired' className="inputText" required />
-                                            <span className="floating-label">Job Requirements</span>
-                                        </div>
-                                    </div>
-                                    <div className="col-12">
-                                        <div className="postbox__comment-input mb-30">
-                                            <input type="text" name='jobLocation' className="inputText" required />
-                                            <span className="floating-label">Job Location</span>
-                                        </div>
-                                    </div>
-                                    <div className="col-xxl-12">
-                                        <div className="postbox__btn-box">
-                                            <button type='submit' className="submit-btn w-100">Add job</button>
-                                        </div>
-                                    </div>
+                                    </form>
                                 </div>
-                            </form>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                ) : (
+                    <div className="row gx-0">
+                        <div className="col-xl-7 col-lg-6">
+                            <div className="contact-form-right-warp">
+                                <div className="postbox__comment-form">
+                                    <p style={{ textAlign: 'center', fontSize: '30px' }}>Log In to Add a new job</p>
+                                    <form className="box" onSubmit={handleLogin}>
+                                        <div className="row gx-20">
+                                            <div className="col-12">
+                                                <div className="postbox__comment-input mb-30">
+                                                    <input type="password" name='adminPassword' className="inputText" required />
+                                                    <span className="floating-label">Admin password</span>
+                                                </div>
+                                            </div>
+                                            <div className="col-xxl-12">
+                                                <div className="postbox__btn-box">
+                                                    <button type='submit' className="submit-btn w-100">Login</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     )
 }
